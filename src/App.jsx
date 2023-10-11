926e4b2480c3a116d14e5d96d63ba2a1653743d9
maximaldo <130598408+maximaldo@users.noreply.github.com>
2023-10-10 23:15:06 -0600

    Add files via upload
    LOCALSTORAGE Y RUTAS BIEN HECHAS, ROAD TO 10 CON TRINI

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
-import './App.css';
-import Markdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-import Blog from './components/blog';
-import {Link} from 'react-router-dom'
-import {  
-  Routes, 
-  Route 
-} from 'react-router-dom';
-import { useState } from 'react'
+import "./App.css";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import Blog from "./components/blog";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 function App() {
-  const [arr,setArr] = useState([])
+
+  const [arr, setArr] = useState([]);
+
+  useEffect(() => {
+    let cosasGuardadas = JSON.parse(localStorage.getItem('post'));
+    if (cosasGuardadas) setArr(cosasGuardadas);
+  }, []);
+
   return (
     <>
-         
-         <Routes>
-         <Route path="/" element={<Link to="/markdown">Ir a pagina de Markdown</Link>}/>
-         <Route exact path='/markdown' element={<Blog arr={arr} setArr={setArr}/>} />
-      </Routes>
-       {arr.map( cosa => (
-       <>
-    <h1>{cosa.title}</h1>
-    <h2>{cosa.name}</h2>
-    <Markdown remarkPlugins={[remarkGfm]}>{cosa.text.substring(0,3)}</Markdown> 
-</>
-   ))}
-       
+
+<Link to="/blog">Ir a pagina de Markdown</Link>
+
+  
+      {arr.map((cosa) => (
+        <>
+          <h1>{cosa.title}</h1>
+          <h2>{cosa.name}</h2>
+          <Markdown remarkPlugins={[remarkGfm]}>{cosa.text}</Markdown>
+        </>
+      ))}
     </>
   );
 }
