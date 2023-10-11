fa572ff9375017d8bca187abb1ffc46584a44120
maximaldo <130598408+maximaldo@users.noreply.github.com>
2023-10-10 23:13:17 -0600

    Add files via upload
    Actualizacion del localStorage y las rutas.Road to comentarios

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import {Link} from 'react-router-dom'
-import { 
-    BrowserRouter, 
-    Routes, 
-    Route 
-  } from 'react-router-dom';
+import { useState,useEffect} from "react";
 
 
 function Blog(props) {
-const { arr, setArr } = props;
 const [texto,setTexto] = useState("")
 const [nombre,setNombre] = useState("")
 const [titulo,setTitulo] = useState("")
+const [arr, setArr] = useState([]);
+
+useEffect(() => {
+  let cosasGuardadas = JSON.parse(localStorage.getItem('post'));
+  if (cosasGuardadas) setArr(cosasGuardadas);
+}, []);
 
 
 
@@ -19,8 +18,9 @@ const [titulo,setTitulo] = useState("")
 function HandleSubmit(e){
 e.preventDefault()
 let cont = {text : texto, title : titulo , name : nombre}
-setArr([...arr, cont])
-console.log([...arr, cont]);
+let cosasActualizadas = [...arr, cont];
+setArr(cosasActualizadas);
+localStorage.setItem('post', JSON.stringify(cosasActualizadas));
 setNombre("")
 setTitulo("")
 setTexto("")
@@ -28,15 +28,12 @@ setTexto("")
     
     return( 
     <>
-             <Routes>
-       <Route path="/" element={<Link to="/">Ir a pagina a HOME</Link>}/>
-      </Routes>
     <form onSubmit={HandleSubmit}>
     <input value={nombre} type="text" placeholder='Escribi tu nombre' onChange={e => setNombre(e.target.value)}/>
     <input  value={titulo} type="text" placeholder='escribi tu titulo' onChange={e => setTitulo(e.target.value)}/>
     <hr />
    
-   <textarea value={texto} placeholder='Escribi tu Blog' onChange={e => setTexto(e.target.value)} cols="40" rows="10"/>
+   <textarea value={texto} placeholder='Escribi tu Blog' onChange={e => setTexto(e.target.value)} cols="30" rows="10"/>
    <br></br>
    <button>Create form</button>
    </form>
